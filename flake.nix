4a0240c37e6d89d06cb356481cca8cb9658c1bfc
Oliver Davies <oliver@oliverdavies.uk>
2023-11-14 21:31:59 +0000

    build: switch to pnpm

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
 
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         devshells.default = {
-          packages = with pkgs; [ "awscli" "nodejs_20" ];
+          packages = with pkgs; [ "awscli" "nodejs_20" "nodePackages.pnpm" ];
         };
       };
     };
