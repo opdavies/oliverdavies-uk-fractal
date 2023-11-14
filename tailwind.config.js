7a78831351ff0445d50dfa2dc7c01d4592e72d52
Oliver Davies <oliver@oliverdavies.uk>
2023-11-14 23:06:29 +0000

    chore: update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,54 @@
+const colors = require("./colours");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const { fontFamily } = defaultTheme;
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    './components/**/*.twig'
-  ],
-  darkMode: false, // or 'media' or 'class'
+  important: true,
+  content: ['./components/**/*.twig'],
   theme: {
+    outline: {
+      black: "1px solid black",
+      white: "1px solid white",
+    },
     extend: {
+      colors,
       fontFamily: {
         sans: [
-          '-apple-system',
-          'BlinkMacSystemFont', 
-          'Segoe UI',
-          'Roboto',
-          'Helvetica',
-          'Arial',
-          'sans-serif',
-          'Apple Color Emoji',
-          'Segoe UI Emoji',
-          'Segoe UI Symbol'
-        ]
-      }
+          "Roboto Condensed",
+          "Arial",
+          "Helvetica Neue",
+          "Helvetica",
+          "sans-serif",
+        ],
+        mono: [
+          "ui-monospace",
+          "SFMono-Regular",
+          "SF Mono",
+          "Consolas",
+          "Liberation Mono",
+          ...fontFamily.mono,
+        ],
+      },
+      spacing: {
+        "2px": "2px",
+      },
+      borderWidth: {
+        3: "3px",
+      },
+      width: {
+        96: "24rem",
+      },
     },
   },
-  plugins: [],
-}
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    require("./assets/tailwindcss/plugins/focus-visible.cjs"),
+    require("@tailwindcss/nesting"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
